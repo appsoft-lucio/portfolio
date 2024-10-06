0553627cfd311477cebfd000a5c3517389c5ea88
appsoft-lucio <luciocdesouza@gmail.com>
2024-10-06 09:53:06 -0300

    Refazendo

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
+// Função para alternar a exibição do menu em dispositivos móveis
 function clickMenu() {
   var listaMenu = document.getElementById("listaMenu");
-  listaMenu.classList.toggle("menu-ativo"); // Alterna a classe 'menu-ativo'
+  listaMenu.classList.toggle("menu-ativo"); // Alterna a classe 'menu-ativo' para exibir ou ocultar o menu
 }
